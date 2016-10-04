a2f1ee991646eba0c9ed61eee5411ecf6cf85612
willforcecomputers <willforcecomputers@gmail.com>
2016-10-04 16:01:29 -0700

    Initial work on functions

---
+++ base64.c
@@ -0,0 +1,38 @@
+/*
+* base64.c
+*
+* CSCI 247, Fall 2016, Lab Exercise 2
+*
+* William Metzger
+*
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main(int argcount, char *arg[]) {
+
+
+}
+
+char stringy(char *args[]) {
+	char string[];
+	int i;
+
+	for(i=0; args[i] !='\0'; i++) {
+
+
+	}
+
+	return string;
+}
+
+int binary_convert(*text) {
+	int bin;
+
+
+	return bin;
+}
